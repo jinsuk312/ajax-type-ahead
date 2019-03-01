7555bf4cb1a58db8066c4f434629ff56eaaa177b
jinsukmik <jinsukmik@gmail.com>
2019-02-28 20:31:18 -0600

    grabbing our data

--- app.js
+++ app.js
@@ -5,3 +5,21 @@ const cities = [];
 fetch(endpoint)
 .then(blob => blob.json())
 .then(data => cities.push(...data))
+
+function findMatches(wordToMatch, cities){
+    return cities.filter(place => {
+        //find out if city or state matches search
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.city.match(regex) || place.state.match(regex);
+    })
+}
+
+function displayMatches(){
+    const matchArray = findMatches(this.value, cities)
+    console.log(matchArray);
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
